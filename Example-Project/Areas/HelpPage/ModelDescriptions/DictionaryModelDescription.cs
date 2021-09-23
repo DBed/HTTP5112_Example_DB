dfea785e5827c3c2973f83454333ff4e5053f0f5
Dylan <bednar.dylan@gmail.com>
2021-09-23 08:55:39 -0400

    Add project files.

---
+++ Example-Project/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Example_Project.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Example-Project/Controllers/FunController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Example_Project.Controllers
+{
+    public class FunController : ApiController
+    {
+        //create some methods
+        //GET api/fun{id}
+        //OUTPUT: Hello
+        public string Get(int id) 
+        {
+            var half = id / 2;
+            string message =  "Half of " + id.ToString() + " is " + half.ToString();
+            return message;
+        }
+        //GET api/fun
+        public string Post() 
+        {
+            return "Hello - you made a POST call";
+        }
+    }
+}
